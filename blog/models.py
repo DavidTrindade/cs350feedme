63f61241729e63ebbd7f4875899e47682d61f449
DavidTrindade <d.ferreira@warwick.ac.uk>
2020-11-15 17:26:47 +0000

    Create models for feeds

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Feed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.CharField(max_length=2000)
+    title = models.TextField()
+    desc = models.TextField()
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    title = models.TextField()
+    desc = models.TextField()
